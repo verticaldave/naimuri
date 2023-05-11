1415e9fe2f1b1fc5e5a641cbebad8539b757c9b7
verticaldave <dyarwood72@googlemail.com>
2023-05-11 17:52:51 +0100

    Update WordSquare.java
    Fix whitespace

--- src/main/java/com/naimuri/wordsquare/WordSquare.java
+++ src/main/java/com/naimuri/wordsquare/WordSquare.java
@@ -9,31 +9,28 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class WordSquare {
-		
-	public List<AcceptedWord> generate(String initString) {
-	  
-	  WordSquareConfig config = WordSquareConfig.init(initString);
 
+  public List<AcceptedWord> generate(String initString) {
+    
+    WordSquareConfig config = WordSquareConfig.init(initString);
     System.out.println(
         format("Configured a %1$d x %1$d word square with allowed characters of %2$s",
                config.getWordLength(), 
                config.getAllowedCharacters() 
         )
     );
-	  
+  
     WordFinder finder = new WordFinder(config.getDictionary());
     RegexBuilder regexBuilder = new RegexBuilder(config.getWordLength());
     UsedCharacterHandler disposal = new UsedCharacterHandler();
 
     String availableCharacters = config.getAllowedCharacters();
-	  List<AcceptedWord> acceptedWords = new ArrayList<>();
-	  Map<Integer, List<String>> checkedWords = new HashMap<>();
-	  
-	  Integer wordIndex = 0;
+    List<AcceptedWord> acceptedWords = new ArrayList<>();
+    Map<Integer, List<String>> checkedWords = new HashMap<>();
+    Integer wordIndex = 0;
 	  	  	      
     while (wordIndex < config.getWordLength()) {
       try {
-
         String regex = regexBuilder.buildSearchRegex(availableCharacters, acceptedWords, wordIndex);
         String foundWord = finder.findWord(regex, checkedWords, availableCharacters, wordIndex);
 
@@ -54,9 +51,9 @@ public class WordSquare {
         }
       }
     }
+	
+    acceptedWords.stream().forEach(e -> System.out.println(e.getWord()));
   	
-  	acceptedWords.stream().forEach(e -> System.out.println(e.getWord()));
-  	
-  	return acceptedWords;
-	}
+    return acceptedWords;
+  }
 }
